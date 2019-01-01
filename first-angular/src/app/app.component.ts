7f061173c55695b0d311978a17182ca810989aa8
ysxmsd <32731899+ysxmsd@users.noreply.github.com>
2019-01-01 18:41:17 +0800

    heroes interface

--- first-angular/src/app/app.component.ts
+++ first-angular/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First Angular App';
+  title = 'Tour Of Heroes';
+  h2='this is title 2';
 }
